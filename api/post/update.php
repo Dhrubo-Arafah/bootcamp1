87957a7f43dd20ee00cd6de7619562ec50c0093e
samiulpro <samiul15-13051@diu.edu.bd>
2022-01-06 22:52:49 +0600

    Update post feature added

---
+++ api/post/update.php
@@ -0,0 +1,33 @@
+<?php
+include_once '../../libs/Headers.php';
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+include_once '../../libs/Result.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate blog post object
+$post = new Post($db);
+
+// Blog post query
+$data = json_decode(file_get_contents("php://input"));
+
+// Set ID to update
+$post->id = $data->id;
+$post->title = $data->title;
+$post->body = $data->body;
+$post->author = $data->author;
+$post->category_id = $data->category_id;
+
+// Create Post
+if ($post->create()) {
+    echo json_encode(
+        array('message' => 'Post Updated')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Post was not Updated')
+    );
+}
